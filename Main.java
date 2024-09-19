3382149365d806165584bcc47639018d9bad3bfc
Thanh0721 <thanhn2789@nycstudents.net>
2024-09-19 09:17:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
 public class Main
-{}
+{
+    public static void main(String[]args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f);
+    
+        System.out.println(f.currentFood);
+        //System.out.println(f.currentFood);
+    
+        System.out.println(f.getCurrentFood());
+    
+        Feeder g = new Feeder(500);
+    }
+    
+}
