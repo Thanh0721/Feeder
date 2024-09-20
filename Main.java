d6386b89cd79ac0c8967b2a031918e2f8c65591c
Thanh0721 <thanhn2789@nycstudents.net>
2024-09-19 22:40:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,25 @@ public class Main
         Feeder f = new Feeder();
         System.out.println(f);
     
-        System.out.println(f.currentFood);
-        //System.out.println(f.currentFood);
-    
-        System.out.println(f.getCurrentFood());
-    
         Feeder g = new Feeder(500);
+        System.out.println("Initial food in g: " + g.getCurrentFood());
+        
+        g.simulateOneDay(12);
+        System.out.println("Food after one day: " + g.getCurrentFood());
+        
+        // Simulate many days with birds and display the result
+        int daysWithFood = g.simulateManyDays(12, 10);
+        System.out.println("Days with food: " + daysWithFood);
+        
+        // Test with another feeder
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+
+        // Test the toString method explicitly
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
     }
     
 }
